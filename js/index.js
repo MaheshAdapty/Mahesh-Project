9d69c027bc8436f4980d45bd3cca520201a86130
Mahesh Londhe <mahesh.londhe@adapty.com>
2019-07-10 12:29:18 +0530

    Updated project files

---
+++ js/index.js
@@ -0,0 +1,28 @@
+// $('.slider-area').slick({
+//     dots: true,
+//     infinite: true,
+//     speed: 500,
+//     fade: true,
+//     cssEase: 'linear'
+//     });
+
+
+// $(document).ready(function() {
+// 	$('.slider-area').slick({
+//    slidesToShow: 3,
+//   slidesToScroll: 1,
+//   autoplay: true,
+//   autoplaySpeed: 5000,
+//   dots: true,
+//   arrows:true,
+//   prevArrow:'<button type="button" class="slick-prev"></button>',
+//   nextArrow:'<button type="button" class="slick-next"></button>'
+//   });
+// 	$('.slide').click(function() {
+//     $slideshow.slick('slickNext');
+//   		});
+//     });
+    
+
+
+
